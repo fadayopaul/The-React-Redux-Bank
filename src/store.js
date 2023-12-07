27488e263c0beb6d897d2fdb00e85ab5d7bcd63e
Paul Fadayo <paulfadayo@yahoo.com>
2023-12-07 18:29:52 +0100

    test: :construction: add more states and conventions

--- src/store.js
+++ src/store.js
@@ -1,14 +1,20 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 
 // Create STate
-const initiaState = {
+const initiaStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
 
-// Define the reducer function
-function reducer(state = initiaState, action) {
+const initiaStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+// Define the reducer function for Accounts
+function accountReducer(state = initiaStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
       return {
@@ -25,7 +31,6 @@ function reducer(state = initiaState, action) {
     case "account/requestLoan":
       if (state.loan > 0) return state;
 
-      // LATER:
       return {
         ...state,
         loan: action.payload.amount,
@@ -45,10 +50,45 @@ function reducer(state = initiaState, action) {
   }
 }
 
-// import create store from redux and store in a variable
-const store = createStore(reducer);
+// Define the reducer function for customers
+function customerReducer(state = initiaStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomers":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
 
+    case "customer/updateName":
+      return {
+        ...state,
+        fullName: action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
+
+/* import create store from redux and store in a variable, 
+then pass the reducer function as an argument into the  `createStore()` function, making it the thr root reducer :
+const store = createStore(accountReducer);
+
+but
+
+if we have two or more reducers, we can combine using the `combineReducers()`, which will receive the reducers as objects and stored into a vairiable. the variable is then passed into the createStore()
+*/
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
+const store = createStore(rootReducer);
 // dispatching the actions from the reducer function
+
 // store.dispatch({ type: "account/deposit", payload: 500 });
 // store.dispatch({ type: "account/withdraw", payload: 200 });
 
@@ -60,8 +100,31 @@ const store = createStore(reducer);
 // store.dispatch({ type: "account/payLoan" });
 // console.log(store.getState());
 
-// Action creators
+/*
+older codebase
 
+const ACCOUNT_DEPOSIT = "account/deposit"
+
+in the reducer function: 
+
+case ACCOUNT_DEPOSIT:
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+      };
+
+
+      in the action creator:
+      function deposit(amount) {
+  return {
+    type: ACCOUNT_DEPOSIT,
+    payload: amount,
+  };
+}
+store.dispatch(deposit(1000));
+ */
+
+// Action creators for Account
 // deposit action
 function deposit(amount) {
   return {
@@ -80,4 +143,50 @@ function withdraw(amount) {
 }
 store.dispatch(withdraw(500));
 
+// requesrLoan action
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: {
+      amount,
+      purpose,
+    },
+  };
+}
+store.dispatch(requestLoan(200, "Setting up a business"));
+
+//  payLoan action
+function payLoan() {
+  return {
+    type: "account/payLoan",
+  };
+}
+store.dispatch(payLoan());
+
+// console.log(store.getState());
+
+//
+// Action creators for customers
+// create customers
+function createCustomers(fullName, nationalID) {
+  return {
+    type: "customer/createCustomers",
+    payload: {
+      fullName,
+      nationalID,
+      createdAt: new Date().toISOString(),
+    },
+  };
+}
+store.dispatch(createCustomers("Paul Fadayo", "Nigerian"));
+console.log(store.getState());
+
+// update customer name
+function updateName(fullName) {
+  return {
+    type: "customer/updateName",
+    payload: fullName,
+  };
+}
+store.dispatch(updateName("Jegede Olasunkanmi"));
 console.log(store.getState());
